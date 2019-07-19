af6cb8c150a88bcf38cd8b3d11bd01830c8ead9b
Maximilian Schmidt <m.schmidt@obis-concept.de>
2019-07-19 12:06:12 +0200

    WIP: Setting up the basic lint command

--- Classes/Command/FusionCommandController.php
+++ Classes/Command/FusionCommandController.php
@@ -5,31 +5,90 @@ namespace MCStreetguy\FusionLinter\Command;
  * This file is part of the MCStreetguy.FusionLinter package.
  */
 
+use Concept\Toolbox\Traits\Command\CommonMessagesTrait;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Cli\CommandController;
+use Neos\Flow\Mvc\Controller\Exception\InactivePackageException;
+use Neos\Flow\Mvc\Exception\InvalidPackageKeyException;
+use Neos\Flow\Package\PackageManagerInterface;
+use Neos\Fusion\View\FusionView;
 
 /**
  * @Flow\Scope("singleton")
  */
 class FusionCommandController extends CommandController
 {
+    use CommonMessagesTrait;
+
+    const EC_INVALID_KEY = 2;
+    const EC_INACTIVE_PACKAGE = 3;
+    const EC_MISSING_PACKAGE = 4;
+    const EC_FROZEN_PACKAGE = 5;
 
     /**
-     * An example command
+     * @var PackageManagerInterface
+     * @Flow\Inject
+     */
+    protected $packageManager;
+
+    /**
+     * @var array
+     * @Flow\InjectConfiguration
+     */
+    protected $settings;
+
+    /**
+     * @var FusionView
+     */
+    protected $fusionView;
+
+    public function initializeObject()
+    {
+        $this->fusionView = FusionView::createWithOptions([
+            'fusionPathPatterns' => ['resource://@package/Private/Fusion'],
+            'fusionPath' => '/root',
+            'debugMode' => true,
+            'enableContentCache' => false,
+        ]);
+    }
+
+    /**
+     * Lint the existing Fusion code.
      *
-     * The comment of this command method is also used for TYPO3 Flow's help screens. The first line should give a very short
-     * summary about what the command does. Then, after an empty line, you should explain in more detail what the command
-     * does. You might also give some usage example.
+     * Lint the existing Fusion code.
      *
-     * It is important to document the parameters with param tags, because that information will also appear in the help
-     * screen.
-     *
-     * @param string $requiredArgument This argument is required
-     * @param string $optionalArgument This argument is optional
+     * @param string $packageKey The package to load the Fusion code from.
+     *                           Must be an active and unfrozen package for the linting to work!
+     * @param string $path The fusion path to be linted.
+     *                     Can be ommitted if the standard '/root' path shall be linted.
      * @return void
      */
-    public function exampleCommand($requiredArgument, $optionalArgument = null)
+    public function lintCommand(string $packageKey, string $path = null)
     {
-        $this->outputLine('You called the example command and passed "%s" as the first argument.', array($requiredArgument));
+        if (!$this->packageManager->isPackageKeyValid($packageKey)) {
+            $this->outputErrorMessage("Package key '$packageKey' is invalid!");
+            $this->quit(self::EC_INVALID_KEY);
+        } elseif (!$this->packageManager->isPackageActive($packageKey)) {
+            if ($this->packageManager->isPackageAvailable($packageKey)) {
+                $this->outputWarningMessage("Package '$packageKey' is not active but available. However only active packages can be linted.");
+                $this->outputInfoMessage("You may want to activate the package with './flow flow:package:activate $packageKey' and try again afterwards.");
+                $this->quit(self::EC_INACTIVE_PACKAGE);
+            }
+
+            $this->outputErrorMessage("Package '$packageKey' could not be found!");
+            $this->quit(self::EC_MISSING_PACKAGE);
+        } elseif ($this->packageManager->isPackageFrozen($packageKey)) {
+            $this->outputWarningMessage("Package '$packageKey' is frozen and thus cannot be linted.");
+            $this->outputInfoMessage("You may want to unfreeze the package with './flow flow:package:unfreeze $packageKey' and try again afterwards.");
+            $this->quit(self::EC_FROZEN_PACKAGE);
+        }
+
+        $this->fusionView->setPackageKey($packageKey);
+        $this->fusionView->setFusionPath($path ?? $this->settings['defaultFusionPath']);
+
+        $result = $this->fusionView->render();
+
+        $this->outputSuccessMessage('No error has been found, your Fusion code seems valid.');
+        $this->quit();
     }
 }
